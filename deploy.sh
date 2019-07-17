a1d5a9567d4a0c7abc3900baf9bb554ffe292505
Yakov Budymko <mopgobopot@gmail.com>
2019-07-17 06:10:52 +0000

    rework sheduler and it params

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ export KRB_REALM=CLOUDERA
 #export END_DATE=2019-07-17T7:10Z
 export NAME_NODE=hdfs://quickstart.cloudera:8020 
 export JOB_TRACKER=quickstart.cloudera:8032
-export SHEDULER="17 * * * *"
+export SHEDULER="0/5 0-23 * * *"
 export HTTPFS_NODE=quickstart.cloudera:14000
 export OOZIE_NODE=quickstart.cloudera:11000
 
